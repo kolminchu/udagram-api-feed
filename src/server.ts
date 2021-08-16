dd60eb70c03a01082779e609c2d6c8249d463e39
scharati@protonmail.com <scharati@protonmail.com>
2021-08-16 15:09:59 +0530

    added logging setup

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import cors from 'cors';
 import express from 'express';
 import {sequelize} from './sequelize';
+import {morgan} from 'morgan';
+import {bunyan} from 'bunyan';
+
 
 import {IndexRouter} from './controllers/v0/index.router';
 
@@ -10,6 +13,19 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
+  let logger = bunyan.createLogger({
+    name: 'feed-api',
+    streams: [{
+      type:'rotating-file',
+      path: '/var/log/api-feed.log',
+      period: '1d',          // daily rotation
+      totalFiles: 10,        // keep up to 10 back copies
+      rotateExisting: true,  // Give ourselves a clean file when we start up, based on period
+      threshold: '10m',      // Rotate log files larger than 10 megabytes
+      totalSize: '20m',      // Don't keep more than 20mb of archived log files
+      gzip: true             // Compress the archive log files to save space
+    }]
+  });
   await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.sync();
 
@@ -17,6 +33,7 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
   const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
+  app.use(morgan('common'));
 
   app.use(cors({
     allowedHeaders: [
